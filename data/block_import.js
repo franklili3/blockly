88f95ab33671d11abf65f4ae69fc41898cb90ca9
franklili <348104201@qq.com>
2020-07-21 20:57:31 +0800

    download block library workspace xml

---
+++ data/block_import.js
@@ -0,0 +1,21 @@
+[{
+  "type": "import",
+  "message0": "%1 %2",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "library_NAME",
+      "text": "math"
+    },
+    {
+      "type": "input_value",
+      "name": "library",
+      "check": "String",
+      "align": "RIGHT"
+    }
+  ],
+  "inputsInline": true,
+  "colour": 230,
+  "tooltip": "导入库",
+  "helpUrl": ""
+}]
