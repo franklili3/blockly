8a38a5c0e366c9c697e3e487d6beb2febd4c9f80
franklili <348104201@qq.com>
2020-07-22 08:55:15 +0800

    add_block_import

---
+++ data/python_import.js
@@ -0,0 +1,6 @@
+Blockly.Python['import'] = function(block) {
+  var value_library_name = Blockly.Python.valueToCode(block, 'library_NAME', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
