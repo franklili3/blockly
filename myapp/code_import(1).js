ea4ff12851c3998271827cd3d9dafb2975ea42a2
franklili <348104201@qq.com>
2020-07-23 19:10:15 +0800

    update_toolbox_block_import

---
+++ myapp/code_import(1).js
@@ -0,0 +1,6 @@
+Blockly.Python['import'] = function(block) {
+  var value_library = Blockly.Python.valueToCode(block, 'library', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
